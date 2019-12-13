d021a4952c383c0038f3ac4aa5a1af548e012743
Filipe Câncio <filipe.cancio@outlook.com>
2019-12-12 21:00:51 -0300

    Update project

--- src/pages/Mensagem.js
+++ src/pages/Mensagem.js
@@ -4,9 +4,22 @@ import { Control, Field, Button, Input } from 'rbx';
 
 class Mensagem extends React.Component {
 
-  state = {
-    boasvindas: 'oi! Aqui é o mensagem.'
-  };
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Um nome foi enviado: ' + this.state.value);
+    event.preventDefault();
+  }
 
   render() {
     const mensagemSection = {
@@ -30,14 +43,16 @@ class Mensagem extends React.Component {
         <section style={mensagemSection}>
           <div style={historico}></div>
           <div style={mensagem}>
+          <form onSubmit={this.handleSubmit}>
             <Field kind="group">
               <Control expanded>
-                <Input placeholder="Digite a mensagem" />
+                <Input value={this.state.value} onChange={this.handleChange} />
               </Control>
               <Control>
-                <Button color="info">Enviar</Button>
+                <Button type="submit"  color="info">Enviar</Button>
               </Control>
             </Field>
+            </form>
           </div>
         </section>
       </div>
